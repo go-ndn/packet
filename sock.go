c2360407ce04bc1be35c5e262f60b3fe4188567e
taylorchu <tailinchu@gmail.com>
2015-05-08 17:20:45 -0700

    move reader to a new file

--- sock.go
+++ sock.go
@@ -1,9 +1,7 @@
 package packet
 
 import (
-	"io"
 	"net"
-	"sync"
 	"syscall"
 	"time"
 )
@@ -14,79 +12,6 @@ const (
 	packetSize = 8192
 	bufferSize = 131072
 )
-
-type entry struct {
-	time time.Time
-	ch   chan byte
-	mu   sync.Mutex
-}
-
-type reader struct {
-	m  map[string]*entry
-	mu sync.Mutex
-}
-
-func newReader() *reader {
-	return &reader{m: make(map[string]*entry)}
-}
-
-func (r *reader) read(b []byte, addr net.Addr) (n int, err error) {
-	var saddr string
-	if addr != nil {
-		saddr = addr.String()
-	}
-	r.mu.Lock()
-	ent, ok := r.m[saddr]
-	r.mu.Unlock()
-	if !ok {
-		err = io.EOF
-		return
-	}
-	for n = 0; n < len(b); n++ {
-		select {
-		case b[n] = <-ent.ch:
-		case <-time.After(Dead):
-			ent.mu.Lock()
-			ok := time.Since(ent.time) < Dead
-			ent.mu.Unlock()
-			if n == 0 && !ok {
-				r.mu.Lock()
-				delete(r.m, saddr)
-				r.mu.Unlock()
-
-				err = io.EOF
-			}
-			return
-		}
-	}
-	return
-}
-
-func (r *reader) write(b []byte, addr net.Addr) (create bool) {
-	var saddr string
-	if addr != nil {
-		saddr = addr.String()
-	}
-	r.mu.Lock()
-	if _, ok := r.m[saddr]; !ok {
-		r.m[saddr] = &entry{
-			ch: make(chan byte, bufferSize),
-		}
-		create = true
-	}
-	ent := r.m[saddr]
-	r.mu.Unlock()
-	if cap(ent.ch)-len(ent.ch) < len(b) {
-		return
-	}
-	ent.mu.Lock()
-	ent.time = time.Now()
-	ent.mu.Unlock()
-	for _, bb := range b {
-		ent.ch <- bb
-	}
-	return
-}
 
 type Conn struct {
 	r      *reader
